984cd126278077809220010f745c8ae586116c4c
supakrit <supakrit@supakrits-MacBook-Pro.local>
2022-05-27 11:14:21 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import MeetupItem from "./components/MeetupItem.svelte";
+
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subTitle: "Learn to code in 2 hours",
+      description: "In this meetup , we will have some experts that teach",
+      imageUrl: "",
+      address: "27th Nerd Road, 32523 New York",
+      contactmail: "code@test.com",
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subTitle: "Let's go for some swimming",
+      description: "We will simply swim some rounds",
+      imageUrl: "",
+      address: "27th Nerd Road, 32523 New York",
+      contactmail: "swim@test.com",
+    },
+  ];
+</script>
+
+<div class="meetup-app">
+  <Header />
+
+  <div class="meetup-list">
+    {#each meetups as meetup (meetup.id)}
+      <MeetupItem
+        title={meetup.title}
+        subTitle={meetup.subTitle}
+        description={meetup.description}
+        imageUrl={meetup.imageUrl}
+        address={meetup.address}
+        contactmail={meetup.contactmail}
+      />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .meetup-list {
+    margin-top: 4rem;
+  }
+</style>
---
+++ src/components/MeetupItem.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let title;
+  export let subTitle;
+  export let description;
+  export let imageUrl;
+  export let address;
+  export let contactmail;
+</script>
+
+<div class="meetup-item">
+  <h1>{title}</h1>
+  <h3>{subTitle}</h3>
+
+  {description}
+  <p>Address : {address}</p>
+  <p>Contact : {contactmail}</p>
+</div>
+
+<style>
+  .meetup-item {
+    padding: 2em;
+    border: 1px solid #ccc;
+    margin-bottom: 1em;
+  }
+</style>
