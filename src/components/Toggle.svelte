798aaa493b5a198ed3faa0b28a4ef46bcc3f4386
supakrit <supakrit@supakrits-MacBook-Pro.local>
2022-05-31 13:34:10 +0700

    Binding to Element References

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -1,20 +1,23 @@
 <script>
   export let chosenOption = 1;
+  let ref = null;
 </script>
 
-<button
-  disabled={chosenOption === 1}
-  on:click={() => {
-    chosenOption = 1;
-  }}
->
-  Option 1</button
->
-<button
-  disabled={chosenOption === 2}
-  on:click={() => {
-    chosenOption = 2;
-  }}
->
-  Option 2</button
->
+<div bind:this={ref} on:click={() => console.log(ref)}>
+  <button
+    disabled={chosenOption === 1}
+    on:click={() => {
+      chosenOption = 1;
+    }}
+  >
+    Option 1</button
+  >
+  <button
+    disabled={chosenOption === 2}
+    on:click={() => {
+      chosenOption = 2;
+    }}
+  >
+    Option 2</button
+  >
+</div>
